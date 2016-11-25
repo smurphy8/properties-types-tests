88929e31a605e3413ab3b2ec72d658f12dcc0af3
Scott Murphy <scottmurphy09@gmail.com>
2016-11-24 21:06:43 -0600

    Updated repo

--- src/Example/Properties.hs
+++ src/Example/Properties.hs
@@ -1,3 +1,21 @@
+
 module Example.Properties where
 
-import Example.Properties.Internal
+
+
+
+
+newtype ProductNumber = ProductNumber { _unProductNumber :: String}
+
+newtype ProductName = ProductName { _unProductName :: String}
+
+
+data Product = Product {
+ _productNumber       :: ProductNumber,
+ _productName         :: ProductName,
+ _version             :: String,
+ _productCustomer     :: String,
+ _productDescription  :: String,
+ _preparedBy          :: String,
+ _productDate         :: String,
+ _productNotes        :: String }
